6f156484c451f2b47909f00ace8fc773a5d8b912
Daneil Wei <daneilmu1@gmail.com>
2014-07-11 15:11:10 +0800

    chanel trigger in controller

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -6,9 +6,9 @@ class ChatController < WebsocketRails::BaseController
 
   def create
     chat = Chat.new message
-    if chat
-      chat.save
+    if chat.save
       controller_store[:message_count] += 1
+      WebsocketRails[:talk].trigger 'new', chat
       send_message :success, chat, :namespace => :chats
     else
       send_message :fail, chat, :namespace => :chats
